9431c537b3079d08f1b3f9a6cfbda168eb99ed7a
Phillss <wootaeky@outlook.com>
2023-02-17 17:29:29 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QPixmap>
+#include <QSplashScreen>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+//    QPixmap onstart(":/res/onstart01.png");
+//    QSplashScreen * splash=new QSplashScreen;
+//    splash->showMessage(QStringLiteral("SAR图像地面站辅助判读系统"),Qt::AlignBottom|Qt::AlignCenter,Qt::gray);
+//    splash->setFont(QFont("microsoft yahei",30,QFont::Bold));
+//    splash->setPixmap(onstart);
+//    splash->show();
+    a.processEvents();
+
+    MainWindow w;
+    w.show();
+//    splash->finish(&w);
+//    delete splash;
+    return a.exec();
+}
---
+++ mainwindow.cpp
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QElapsedTimer>
+#include <QScreen>
+#include <QRect>
+#define MIN_height 740
+#define MIN_width 580
+#define MAX_height 1200
+#define MAX_width 1600
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QList<QScreen*> list_Screen =QGuiApplication::screens();
+    QRect rect=list_Screen.at(0)->geometry();
+    int desk_width=rect.width();
+    int desk_height=rect.height();
+    int pos_x=desk_width/6,pos_y=desk_height/5,adjust_wid=desk_width*2/3,adjust_hei=desk_height*2/3;
+    setGeometry(pos_x,pos_y,adjust_wid,adjust_hei);
+    setWindowTitle("SAR图像地面站辅助判读系统");
+    setMinimumSize(MIN_height,MIN_width);
+    setMaximumSize(MAX_height,MAX_width);
+//    QElapsedTimer t;
+//    t.start();
+//    while(t.elapsed()<2500)
+//        QCoreApplication::processEvents();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
